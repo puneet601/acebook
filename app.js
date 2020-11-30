c8b4f4ad72a563b54b180580c2d8d91b0d2a95da
Puneet K Jattana <puneetkaurjattana@gmail.com>
2020-11-30 11:50:07 +0530

    cover photo added

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
 require('dotenv').config();
-const express=require('express');
-const app=express();
-const formidable = require("express-formidable");
+var express = require("express");
+var app = express();
+
+var formidable = require("express-formidable");
 app.use(formidable());
-const mongodb = require("mongodb");
-const mongoClient = mongodb.MongoClient;
-const ObjectId = mongodb.ObjectId;
-const http=require("http").createServer(app);
-const bcrypt=require("bcrypt");
-const filesystem=require("fs");
-const jwt=require("jsonwebtoken");
-const accessTokenSecret=process.env.SECRET;
-app.use("/public",express.static(__dirname + "/public"));
-app.set("view engine","ejs");
-const socketIO=require("socket.io")(http);
+
+var mongodb = require("mongodb");
+var mongoClient = mongodb.MongoClient;
+var ObjectId = mongodb.ObjectId;
+
+var http = require("http").createServer(app);
+var bcrypt = require("bcrypt");
+var fileSystem = require("fs");
+
+var jwt = require("jsonwebtoken");
+var accessTokenSecret = process.env.SECRET;
+
+app.use("/public", express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
+var socketIO = require("socket.io")(http);
 var socketID = "";
 var users = [];
- var mainURL = "http://localhost:3000";
- socketIO.on("connection", function (socket) {
-     console.log("User connected", socket.id);
-     socketID=socket.id;
- });
- http.listen(3000,function(){
-     console.log("Server Started at port 3000");
+
+var mainURL = "http://localhost:3000";
+
+socketIO.on("connection", function (socket) {
+	console.log("User connected", socket.id);
+	socketID = socket.id;
+});
+
+http.listen(3000, function () {
+	console.log("Server started at " + mainURL);
+     
     mongoClient.connect("mongodb://localhost:27017",{ useUnifiedTopology: true }, function(error,client){
         var database = client.db("acebook");
         console.log("Database Connected.");
@@ -129,7 +139,82 @@ else{
 app.get("/updateProfile",function(req,res){
     res.render("updateProfile");
 });
+app.get("/",function(req,res){
+res.send("hey");
+});
+app.post("/getUser", function (request, result) {
+    var accessToken = request.fields.accessToken;
+    database.collection("users").findOne({
+        "accessToken": accessToken
+    }, function (error, user) {
+        if (user == null) {
+            result.json({
+                "status": "error",
+                "message": "User has been logged out. Please login again."
+            });
+        } else {
+            result.json({
+                "status": "success",
+                "message": "Record has been fetched.",
+                "data": user
+            });
+        }
+    });
+});
 
+app.get("/logout", function (request, result) {
+    result.redirect("/login");
+});
+
+app.post("/uploadCoverPhoto", function (request, result) {
+    var accessToken = request.fields.accessToken;
+    var coverPhoto = "";
+
+    database.collection("users").findOne({
+        "accessToken": accessToken
+    }, function (error, user) {
+        if (user == null) {
+            result.json({
+                "status": "error",
+                "message": "User has been logged out. Please login again."
+            });
+        } else {
+
+            if (request.files.coverPhoto.size > 0 && request.files.coverPhoto.type.includes("image")) {
+
+                if (user.coverPhoto != "") {
+                    fileSystem.unlink(user.coverPhoto, function (error) {
+                        //
+                    });
+                }
+
+                coverPhoto = "public/images/" + new Date().getTime() + "-" + request.files.coverPhoto.name;
+                fileSystem.rename(request.files.coverPhoto.path, coverPhoto, function (error) {
+                    //
+                });
+
+                database.collection("users").updateOne({
+                    "accessToken": accessToken
+                }, {
+                    $set: {
+                        "coverPhoto": coverPhoto
+                    }
+                }, function (error, data) {
+                    result.json({
+                        "status": "status",
+                        "message": "Cover photo has been updated.",
+                        data: mainURL + "/" + coverPhoto
+                    });
+                });
+            } else {
+                result.json({
+                    "status": "error",
+                    "message": "Please select valid image."
+                });
+            }
+        }
+    });
+});
     });
    
  });
